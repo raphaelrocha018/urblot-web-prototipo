9483a253027ae121cae3e57bb8348e4fa8b2f80b
Raphael Rocha <raphaelrocha18@outlook.com>
2023-08-24 16:07:19 -0300

    update

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const modalLogin = document.querySelector('#modal-login')
+const modalForgetPassword = document.querySelector('#modal-forget-password')
+
+const btnForgetPassword = document.querySelector('#forgetPassword')
+const btnCloseForgetPassword = document.querySelector('#btn-close')
+const btnLinkResetPassword = document.querySelector('#btnLinkResetPassword')
+
+btnForgetPassword.addEventListener('click', openForgetPassword)
+btnCloseForgetPassword.addEventListener('click', closeForgetPassword)
+btnLinkResetPassword.addEventListener('click', sendLinkResetPassword)
+
+function openForgetPassword() {
+    modalLogin.classList.add('hidden')
+    modalForgetPassword.classList.remove('hidden')
+}
+
+function closeForgetPassword() {
+    modalLogin.classList.remove('hidden')
+    modalForgetPassword.classList.add('hidden')
+}
+
+function sendLinkResetPassword(event) {
+    event.preventDefault()
+}
