69fe7c917949459f63924f047a00e03b2b7a2063
Raphael Rocha <raphaelrocha18@outlook.com>
2023-08-25 12:02:09 -0300

    update

--- js/plan-types.js
+++ js/plan-types.js
@@ -1,14 +1,18 @@
+const body = document.querySelector('body')
 const modalPlanTypes = document.querySelector('#modal-plan-types')
 const btnPlanTypes = document.querySelector('#btn-plan-types')
 const btnClose = document.querySelector('#btn-close')
 
 function openPlanTypes() {
+    body.classList.add('stop-scrolling')
     modalPlanTypes.classList.remove('hidden')
 }
 
 function closePlanTypes() {
+    body.classList.remove('stop-scrolling')
     modalPlanTypes.classList.add('hidden')
 }
 
 btnPlanTypes.addEventListener('click', openPlanTypes)
 btnClose.addEventListener('click', closePlanTypes)
+
