b0d20ce4b89dd6bc9807c1b9fa1e0c9ecd509bed
Raphael Rocha <raphaelrocha18@outlook.com>
2023-08-27 18:53:23 -0300

    update

--- js/plan-types.js
+++ js/plan-types.js
@@ -1,19 +1,22 @@
 const html = document.documentElement
 const body = document.querySelector('body')
 const mobile = window.matchMedia("(max-width: 720px)").matches;
-const modalPlanTypes = document.querySelector('#modal-plan-types')
-const btnPlanTypes = document.querySelector('#btn-plan-types')
-const btnClose = document.querySelector('#btn-close')
+
+const PlanTypes = {
+    modal: document.querySelector('#modal-plan-types'),
+    card: document.querySelector('#modal-plan-types > div'),
+    btnOpen: document.querySelector('#btn-plan-types'),
+    btnClose: document.querySelector('#btn-close'),
+}
 
 function openPlanTypes() {
     html.classList.add('stop-scrolling-html')
     body.classList.add('stop-scrolling-body')
-
     if (!mobile) {
         body.classList.add('compensate-scrollbar')
     }
-
-    modalPlanTypes.classList.remove('hidden')
+    PlanTypes.modal.classList.remove('hidden')
+    PlanTypes.card.classList.remove('hidden')
 }
 
 function closePlanTypes() {
@@ -21,7 +24,8 @@ function closePlanTypes() {
     body.classList.remove('stop-scrolling-body')
     body.classList.remove('compensate-scrollbar')
 
-    modalPlanTypes.classList.add('hidden')
+    PlanTypes.modal.classList.add('hidden')
+    PlanTypes.card.classList.add('hidden')
 }
 
 function handleOutsideClick(event) {
@@ -30,6 +34,6 @@ function handleOutsideClick(event) {
     }
 }
 
-btnPlanTypes.addEventListener('click', openPlanTypes)
-btnClose.addEventListener('click', closePlanTypes)
-modalPlanTypes.addEventListener('click', handleOutsideClick)
+PlanTypes.btnOpen.addEventListener('click', openPlanTypes)
+PlanTypes.btnClose.addEventListener('click', closePlanTypes)
+PlanTypes.modal.addEventListener('click', handleOutsideClick)
